d8b3fe9024e0418262081cd1c859003708817b45
TTomes1 <tmtomer@uh.edu>
2020-04-02 11:16:53 -0500

    updated UI buttons

--- SearchDrone/ViewController.swift
+++ SearchDrone/ViewController.swift
@@ -15,10 +15,19 @@ class ViewController: DUXDefaultLayoutViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Stylize buttons
+        missionStart.layer.cornerRadius = 4
+        missionStop.layer.cornerRadius = 4
+        
+        // Initially hide Stop button
+        self.missionStop.isHidden = true
+        
         // Do any additional setup after loading the view.
         self.contentViewController?.view.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0).isActive = true
         self.contentViewController?.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0).isActive = true
         self.contentViewController?.view.setNeedsDisplay()
+        
 
     }
     
