df2b2077f8f6f231d28019ab1702d9c93878fe4b
TTomes1 <tmtomer@uh.edu>
2020-04-02 21:02:39 -0500

    Initial Mission Update

--- SearchDrone/WaypointMission.swift
+++ SearchDrone/WaypointMission.swift
@@ -5,14 +5,14 @@
 //  Created by Travis Tomer on 4/1/20.
 
 
-import Foundation
-import DJISDK
-import DJIWidget
-
-class MissionController: MKMapView {
-
-    
-    
-
-
-}
+//import Foundation
+//import DJISDK
+//import DJIWidget
+//
+//class MissionController: MKMapView {
+//
+//
+//
+//
+//
+//}
